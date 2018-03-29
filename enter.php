2d23d0ba7113b9a0690a18730528423335ee1b41
Pavlo Sholom <pavlo.sholom@gmail.com>
2018-03-29 08:30:30 +0300

    cookies added into selection pannel

--- enter.php
+++ enter.php
@@ -8,10 +8,11 @@
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/main.css">
+
     <title>cURL</title>
   </head>
   <body >
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.min.js"></script>
 
     <div class="container">
           <div class="row centered-form">
@@ -21,45 +22,74 @@
   			    		<h3 class="panel-title">PHP cURL request</h3>
   			 			</div>
   			 			<div class="panel-body">
-  			    		<form>
+
+  			    		<form id="form">
   			    			<div class="row">
   			    				<div class="col-xs-6 col-sm-6 col-md-6">
   			    					<div class="form-group">
-  			                <input type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
+  			                <input type="text" name="first_name" id="first_name" class="form-control input-sm form_input" placeholder="First Name">
   			    					</div>
   			    				</div>
   			    				<div class="col-xs-6 col-sm-6 col-md-6">
   			    					<div class="form-group">
-  			    						<input type="text" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
+  			    						<input type="text" name="last_name" id="last_name" class="form-control input-sm form_input" placeholder="Last Name">
   			    					</div>
   			    				</div>
   			    			</div>
 
   			    			<div class="form-group">
-  			    				<input type="url" name="url" id="url" class="form-control input-sm" placeholder="Url">
+  			    				<input type="url" name="url" id="url" class="form-control input-sm form_input" placeholder="URL">
   			    			</div>
 
                 	<div class="form-group">
                     <label>Request method:</label>
-                    <div class="input-droup">
-                      <label class="radio-inline"><input type="radio" name="optradio">GET</label>
-                      <label class="radio-inline"><input type="radio" name="optradio">POST</label>
-                    </div>
-
-                  </div>
-
-                  <div class="row">
-                    <label class="col-sm-6" for="submit_send">Sending by submit button</label>
-                    <div class="col-sm-6">
-                      <input id="submit_send" type="submit" value="Send" class="input-md">
+                    <div class="radio_row">
+                      <div class="radio_group">
+                        <input  type="radio" name="optradio" value="GET" class="form_input" checked>
+                        <label class="input_label" for="radio1">GET</label>
+                      </div>
+                      <div class="radio_group">
+                        <input  type="radio" name="optradio" value="POST" class="form_input">
+                        <label class="input_label" for="radio2">POST</label>
+                      </div>
                     </div>
                   </div>
 
-                  <div class="row">
-                    <label class="col-sm-6" for="submit_send">Sending by PHP cURL</label>
-                    <div class="col-sm-6">
-                        <input id="button_send" type="button" value="Send" class="input-md">
-                    </div>
+
+                  <div class="select_group">
+
+                      <div class="form-group">
+
+                          <select name="inputs" id="inputs_data" multiple="multiple">
+                            <option value="1">Area, Ethnic</option>
+                             <option value="2">123456</option>
+                             <option value="3">rg1r1gr1</option>
+                             <option value="4">Science</option>
+                             <option value="5">Engineering</option>
+                          </select>
+
+                      </div>
+
+                      <div class="form-group">
+                          <select name="inputs" id="inputs_" multiple="multiple">
+                          </select>
+                      </div>
+
+                  </div>
+
+
+
+
+
+
+                  <div class="button_row">
+                    <label class="input_label" for="submit_send">Sending by submit button (without coocies):</label>
+                    <input id="submit_send" type="submit" value="Send" class="input-md">
+                  </div>
+
+                  <div class="button_row">
+                    <label class="input_label" for="button_send">Sending by PHP cURL:</label>
+                    <input id="button_send" type="button" value="Send" class="input-md">
                   </div>
 
 
@@ -68,11 +98,15 @@
   	    		</div>
       		</div>
       	</div>
+
+
       </div>
 
 
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="/js/request.js"></script>
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+      <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+      <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.min.js"></script>
+      <script src="/js/request.js"></script>
   </body>
 </html>
--- request.php
+++ request.php
@@ -1,7 +1,15 @@
 <?php
 
 
+
+
+
   if ($_GET['button_send'] != "" && $_GET['button_send'] == 'button_send'){
+    setcookie('id','100');
+    setcookie('first_name','David');
+    setcookie('last_name','Kuzava');
+    setcookie('phone','0996357412');
+
     $ch = curl_init($_GET['url']);
     $fp = fopen("example_homepage.txt", "w");
 
@@ -11,8 +19,34 @@
     curl_exec($ch);
     curl_close($ch);
     fclose($fp);
-    echo "File written!";
+    $itemLi = "";
+    $i = 1;
+    foreach($_COOKIE as $key => $value) {
+      $itemLi .=
+      "<li><a tabindex='0'><label class='checkbox'><input type='checkbox' value='$i'>$value</label></a></li>";
+      $i+=1;
+    }
+    $result[] = $itemLi;
+    $itemOpt = "";
+    $i = 1;
+    foreach($_COOKIE as $key => $value) {
+      $itemOpt .=
+      "<option value='$i'>$value</option>";
+      $i+=1;
+    }
+    $result[] = $itemOpt;
+    // echo json_encode($_COOKIE);
+
+    echo json_encode($result);
+
     exit;
   }
+
+  // if ($_GET['inputs_data'] != "" && $_GET['inputs_data'] == 'inputs_data'){
+  //
+  //   echo
+  //   "    ";
+  //   exit;
+  // }
 
 ?>
